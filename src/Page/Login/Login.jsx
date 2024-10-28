3da6b158a3c39659c0cbf640cad59e3007002c6b
Mon-Blacklove <nanthamon.sup@spumail.net>
2024-10-28 20:55:14 +0700

    a14

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -5,45 +5,49 @@ import { verifyUser } from "../../DATA/users";
 import "./Login.css";
 
 
-function Login({setToken,setRole}) {
-const userRef = useRef()
-const passRef = useRef()
+function Login({ setToken, setRole }) {
+  const userRef = useRef()
+  const passRef = useRef()
 
   return (
-    <div className="login-container">
-      <Form.Label htmlFor="username">Username</Form.Label>
-      <Form.Control
-        type="text"
-        id="username"
-        placeholder="user"
-        ref = {userRef}
-      />
-      <Form.Label htmlFor="password">Password</Form.Label>
-      <Form.Control
-        type="password"
-        id="password"
-        placeholder="pass"
-        ref={passRef}
-      />
-      <button className="btn btn-primary mt-3" onClick={() => {
-        const user = userRef.current.value.trim()
-        const pass = passRef.current.value.trim()
-        const userInfo = verifyUser(user,pass)
-        userRef.current.value = ''
-        passRef.current.value = ''    
 
-        if(userInfo === null){
+    <div className="login-form">
+      <div><h1>Login</h1></div>
+      <div className="login-container">
+        <Form.Label htmlFor="username">Username</Form.Label>
+        <Form.Control
+          type="text"
+          id="username"
+          placeholder="user"
+          ref={userRef}
+        />
+        <Form.Label htmlFor="password">Password</Form.Label>
+        <Form.Control
+          type="password"
+          id="password"
+          placeholder="pass"
+          ref={passRef}
+        />
+        <button className="btn btn-primary mt-3" onClick={() => {
+          const user = userRef.current.value.trim()
+          const pass = passRef.current.value.trim()
+          const userInfo = verifyUser(user, pass)
+          userRef.current.value = ''
+          passRef.current.value = ''
 
-          alert('Username or Password is incorrect')
-          userRef.current.focus()
+          if (userInfo === null) {
 
-        }else{
+            alert('Username or Password is incorrect')
+            userRef.current.focus()
+
+          } else {
             setRole(userInfo.role)
             setToken(userInfo.token)
 
-        }
+          }
 
-      }}>Login</button>
+        }}>Login</button>
+      </div>
     </div>
   );
 }
